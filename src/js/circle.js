92278345b7389bad16fff2bc9b5bbbe53586b007
이규성 <dvdpig@naver.com>
2019-07-20 14:40:06 +0900

    second commit

---
+++ src/js/circle.js
@@ -0,0 +1,4 @@
+// commonJS 개별모듈화 => es6 named export
+export var area = (r) => Math.PI * r * r;
+
+export var circumference = (r) => 2*Math.PI * r;
---
+++ src/js/cubic.js
@@ -0,0 +1,4 @@
+// commonJS 전체 모듈화 => es6 default export
+var cubic = x => x * x * x;
+
+export default cubic;
